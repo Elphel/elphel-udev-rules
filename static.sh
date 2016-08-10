327a4e7fb36c4e43d8cfb3678de5e9f73a6978d7
Oleg Dzhimiev <oleg@elphel.com>
2016-08-10 13:22:24 -0600

    path passed as an argument

--- static.sh
+++ static.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-mknod -m 0666   /dev/circbuf0   c 135   32
-mknod -m 0666   /dev/circbuf1   c 135   33
-mknod -m 0666   /dev/circbuf2   c 135   34
-mknod -m 0666   /dev/circbuf3   c 135   35
+mknod -m 0622   $1/circbuf0   c 135   32
+mknod -m 0622   $1/circbuf1   c 135   33
+mknod -m 0622   $1/circbuf2   c 135   34
+mknod -m 0622   $1/circbuf3   c 135   35
 
-mknod -m 0666   /dev/jpeghead0  c 135   48
-mknod -m 0666   /dev/jpeghead1  c 135   49
-mknod -m 0666   /dev/jpeghead2  c 135   50
-mknod -m 0666   /dev/jpeghead3  c 135   51
+mknod -m 0622   $1/jpeghead0  c 135   48
+mknod -m 0622   $1/jpeghead1  c 135   49
+mknod -m 0622   $1/jpeghead2  c 135   50
+mknod -m 0622   $1/jpeghead3  c 135   51
 
-mknod -m 0666   /dev/exif_exif0     c 125   16
-mknod -m 0666   /dev/exif_exif1     c 125   17
-mknod -m 0666   /dev/exif_exif2     c 125   18
-mknod -m 0666   /dev/exif_exif3     c 125   19
+mknod -m 0622   $1/exif_exif0     c 125   16
+mknod -m 0622   $1/exif_exif1     c 125   17
+mknod -m 0622   $1/exif_exif2     c 125   18
+mknod -m 0622   $1/exif_exif3     c 125   19
 
-mknod -m 0666   /dev/exif_meta0     c 125   32
-mknod -m 0666   /dev/exif_meta1     c 125   33
-mknod -m 0666   /dev/exif_meta2     c 125   34
-mknod -m 0666   /dev/exif_meta3     c 125   35
+mknod -m 0622   $1/exif_meta0     c 125   32
+mknod -m 0622   $1/exif_meta1     c 125   33
+mknod -m 0622   $1/exif_meta2     c 125   34
+mknod -m 0622   $1/exif_meta3     c 125   35
 
-mknod -m 0666   /dev/exif_template  c 125   2
-mknod -m 0666   /dev/exif_metadir   c 125   3
+mknod -m 0622   $1/exif_template  c 125   2
+mknod -m 0622   $1/exif_metadir   c 125   3
